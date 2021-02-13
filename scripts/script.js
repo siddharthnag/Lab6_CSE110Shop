5103ece67651b6d49fc9802dd2eb62cb0a5f6822
Siddharth Nag <59549474+siddharthnag@users.noreply.github.com>
2021-02-13 11:56:02 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  fetch("https://fakestoreapi.com/products")
-    .then(response => response.json())
-    .then(data => localStorage.setItem("array", JSON.stringify(data)));
+
 });
