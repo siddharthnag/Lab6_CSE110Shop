1de66ead5fca1d5ecc5bfa74f3f6c0ed71360ccd
Siddharth Nag <59549474+siddharthnag@users.noreply.github.com>
2021-02-13 14:29:39 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -2,14 +2,16 @@
 
 class ProductItem extends HTMLElement {
   constructor() {
-    super();
-    const shadowRoot = this.attachShadow( { mode: "open" });
-    shadowRoot.innerHTML = 
+    const productItem = document.createElement("productItem");
+    productItem.innerHTML = 
       `<img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
       <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
       <p class="price">$109.95</p>
       <button onclick="alert('Added to Cart!')">Add to Cart</button>`
     ;
+    super();
+    this.root = this.attachShadow( { mode: "open" });
+    
   }
 }
 
