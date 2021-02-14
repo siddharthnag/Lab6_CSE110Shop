a1485af1def38a9d69831bdfa5df7e39fbdbe871
Siddharth Nag <59549474+siddharthnag@users.noreply.github.com>
2021-02-13 19:05:14 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,35 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  
+  if (localStorage.getItem("data") == null) {
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => localStorage.setItem("data", JSON.stringify(data)));
+  }
+
+  let products = JSON.parse(localStorage.getItem("data"));
+  let productList = document.getElementById("product-list");
+  let cartCount = document.getElementById("cart-count");
+
+  if (localStorage.getItem("cart") == null) {
+    let cart = [];
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if(JSON.parse(localStorage.getItem('count')) == null) {
+    localStorage.setItem('count', JSON.stringify(cartCount.textContent));
+    count = JSON.parse(localStorage.getItem('count'))
+  } else {
+    count = JSON.parse(localStorage.getItem('count'));
+  }
+  cartCount.textContent = count;
+
+  for (var product in products) {
+    let item = products[product];
+    let productItem = document.createElement("product-item");
+    productItem = new ProductItem(item.id, item.image, item.title, item.price, cart.includes(item.id));
+    productList.appendChild(ProductItem);
+  }
 });
